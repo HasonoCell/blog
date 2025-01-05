64520cd378b0b0873638b9e8b6c2e24e53312a71
HasonoCell <2105689721@qq.com>
2025-01-05 15:12:19 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./docs/.vitepress/**/*.{vue,js}", "./docs/**/*.md"],
-  theme: {
-    extend: {
-      colors: {
-        VPLight: "#3451b2",
-        VPDark: "#a8b1ff",
-      },
+export const content = ["./docs/.vitepress/**/*.{vue,js}", "./docs/**/*.md"];
+export const theme = {
+  extend: {
+    colors: {
+      VPLight: "#3451b2",
+      VPDark: "#a8b1ff",
     },
   },
-  plugins: [],
-  darkMode: "class",
 };
+export const plugins = [];
+export const darkMode = "class";
